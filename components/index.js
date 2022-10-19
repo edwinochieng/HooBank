57a84661150fa4583ebd9387d09729d024617dac
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-10-19 16:55:52 +0300

    initial commit

---
+++ components/index.js
@@ -0,0 +1,24 @@
+import Navbar from './Navbar'
+import Billing from './Billing'
+import Stats from './Stats'
+import Hero from './Hero'
+import Testimonials from './Testimonials'
+import Footer from './Footer'
+import CTA from './CTA'
+import CardDeal from './CardDeal'
+import Business from './Business'
+import Clients from './Clients'
+
+export {
+    Navbar,
+    Billing,
+    Stats,
+    Hero,
+    Testimonials,
+    Footer,
+    CTA,
+    CardDeal,
+    Business,
+    Clients
+}
+
