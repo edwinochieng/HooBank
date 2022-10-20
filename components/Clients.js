ce216489713c96126f75da21ff1c519df9e437e5
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-10-21 01:25:36 +0300

    client logos

--- components/Clients.js
+++ components/Clients.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { clients } from '../constants'
+import Image from 'next/image'
 
 function Clients() {
   return (
-    <div>Clients</div>
+    <div className='flex justify-center items-center my-4'>
+     <div className='flex justify-center items-center flex-wrap w-full'>
+        {clients.map((client) => (
+          <div key={client.id} className="flex-1 flex justify-center items-center sm:min-w-[192px] min-w-[120px]">
+            <Image src={client.logo} alt="client" className='sm:w-[192px] w-[100px] object-contain'/>
+          </div>
+        ))}
+     </div>
+    </div>
   )
 }
 
