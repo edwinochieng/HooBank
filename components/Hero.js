3d34d41dd44ef37fc06b8c499a671f60116740b8
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-10-20 15:58:14 +0300

    hero image + overlays

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { discount } from '../public/assets'
+import { discount, robot } from '../public/assets'
 import Image from 'next/image'
 import GetStarted from './GetStarted'
 
@@ -26,6 +26,15 @@ function Hero() {
         </div>
         <h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>Payment Method.</h1>
         <p className='font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5'>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs . We examine annual percentage rates, annual fees. </p>
+      </div>
+      <div className='flex-1 flex justify-center items-center md:my-0 my-10 relative'>
+        <Image src={robot} alt="billing" className='h-[100%] w-[100%] relative z-[5]'/>
+        <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
+        <div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient'/>
+        <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient'/>
+      </div>
+      <div className='sm:hidden flex justify-center items-center'>
+        <GetStarted/>
       </div>
     </div>
   )
