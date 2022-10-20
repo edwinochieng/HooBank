e90b6d81f72ee538dfabff10de8b3fed1c219fce
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-10-20 20:11:57 +0300

    get started button

---
+++ components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <button type='button' className='bg-blue-gradient py-4 px-6 mt-10 rounded-lg font-poppins font-medium text-[18px] text-primaary outline-none'>Get Started</button>
+  )
+}
+
+export default Button
